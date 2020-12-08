41e484a8d4c591cbdc5485e213fde533dcb3feb7
Ubuntu <ubuntu@ip-172-31-16-17.ec2.internal>
2020-12-08 03:27:55 +0000

    demo-1 added

---
+++ demo-1/instance.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "test-instance" {
+ name         = "test-instance"
+ machine_type = "f1-micro"
+ zone         = "us-central1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "ubuntu-os-cloud/ubuntu-1604-lts"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt-get update"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
